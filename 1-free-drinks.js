02f7e7fe606aa78c7c4ead18ad24db45f4287b83
Chamak Saha <chamaksaha58@gmail.com>
2024-09-03 21:55:52 +0600

    uploaded task: free drinks

---
+++ 1-free-drinks.js
@@ -0,0 +1,15 @@
+/***
+
+Free Drinks
+    - Burger more than 500tk: free Coke
+    - Else Coke: 30tk
+*/
+
+let burgerPrice = 600;
+
+if(burgerPrice > 500){
+    console.log('Congratulations! You got a free Coke.')
+}
+else{
+    console.log('You can buy this Coke by paying tk30.')
+}

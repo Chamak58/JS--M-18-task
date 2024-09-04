d3b3a6cb3906d6138415ccfe50958a18a9fd9dcd
Chamak Saha <chamaksaha58@gmail.com>
2024-09-04 10:48:51 +0600

    uoloaded task: bus-fare-calculator.js file

--- 6-bus-fare-calculator.js
+++ 6-bus-fare-calculator.js
@@ -6,3 +6,39 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+
+//--------if/else-if/else----------
+
+let fare = 500;
+let children = false;
+let age = 68;
+let students = false;
+let seniorCitizens = false;
+
+if(children === true && age <= 10){
+    console.log('Free')
+}
+else if(students === true){
+    console.log(fare - [fare * 50 / 100])
+}
+else if(seniorCitizens === true && age >= 60){
+    console.log(fare - [fare * 15 / 100])
+}
+else{
+    console.log(fare)
+}
+//------------ternary---------
+
+// let children = true;
+// let age = 8;
+// fare = children === true && age <= 10? 'Free' : fare;
+// console.log(fare)
+
+// let student = true;
+// fare = student === true ? fare * 50 / 100 : fare;
+// console.log(fare)
+
+// let seniorCitizens = true;
+// let age = 75;
+// fare = seniorCitizens === true && age >= 60 ? fare - [fare * 15 / 100] : fare;
+// console.log(fare)
